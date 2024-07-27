7ae503fe9c97b0cb5a331efd180459322ba61127
Root23 <168601444+renesido@users.noreply.github.com>
2024-07-26 21:45:42 -0500

    update views/register.php

--- views/register.php
+++ views/register.php
@@ -3,11 +3,11 @@
 <html>
 <head>
   <meta http-equiv="CONTENT-TYPE" content="text/html; charset=UTF-8">
-  <title></title></title>
+  <title>registro</title>
 </head>
 <body>
   <h1>
-    Hello, World!
+    Hello, Worl
   </h1>
 </body>
 </html>
